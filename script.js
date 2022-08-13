a69703ff6164a6d6b831025797b508736b967396
Natasha Chiorsac <75411738+natashachiorsac@users.noreply.github.com>
2022-08-14 00:56:15 +1000

    implemented scoring system

--- script.js
+++ script.js
@@ -8,36 +8,107 @@ function getPlayerChoice(){
     return choice.toLowerCase();
 }
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return "It's a draw";
-    }
-
-    switch (playerSelection) {
+function displayPlayerMove(move){
+    switch (move){
         case "paper":
-            if (computerSelection == "scissors"){
-                return "You lose :(";
-            }
-            else{
-                return "You win!";
-            }
+            document.getElementById('player-move').src="paper.png";
+            break;
 
         case "scissors":
-            if (computerSelection == "rock"){
-                return "You lose :(";
-            }
-            else{
-                return "You win!";
-            }
+            document.getElementById('player-move').src="scissors.png";
+            break;
 
         case "rock":
-            if (computerSelection == "paper"){
-                return "You lose :(";
-            }
-            else{
-                return "You win!";
-            }
+            document.getElementById('player-move').src="rock.png";
+            break;
+
     }
+}
+
+function displayComputerMove(move){
+    switch (move){
+        case "paper":
+            document.getElementById('computer-move').src="paper.png";
+            break;
+
+        case "scissors":
+            document.getElementById('computer-move').src="scissors.png";
+            break;
+
+        case "rock":
+            document.getElementById('computer-move').src="rock.png";
+            break;
+
+    }
+}
+
+function displayScores(playerScore, computerScore){
+    document.getElementById('player-score-text').textContent=playerScore;
+    document.getElementById('computer-score-text').textContent=computerScore;
+}
+
+let gameOver = false;
+let playerScore = 0;
+let computerScore = 0;
+let message = "Make your move to start the game";
+
+function playRound(playerSelection, computerSelection){
+    if (!gameOver){
+        if (playerSelection == computerSelection){
+            message = "It's a draw";
+        } else {
+            switch (playerSelection) {
+                case "paper":
+                    if (computerSelection == "scissors"){
+                        computerScore++;
+                        message = "You lose :(";
+                    }
+                    else{
+                        playerScore++;
+                        message = "You win!";
+                    }
+                    break
+
+                case "scissors":
+                    if (computerSelection == "rock"){
+                        computerScore++;
+                        message = "You lose :(";
+                    }
+                    else{
+                        playerScore++;
+                        message = "You win!";
+                    }
+                    break
+
+                case "rock":
+                    if (computerSelection == "paper"){
+                        computerScore++;
+                        message = "You lose :(";
+                    }
+                    else{
+                        playerScore++;
+                        message = "You win!";
+                    }
+                    break
+            }
+        }
+
+        displayScores(playerScore, computerScore);
+        displayPlayerMove(playerSelection);
+        displayComputerMove(computerSelection);
+
+        if (playerScore==5){
+            gameOver = true;
+            document.getElementById('results').setAttribute('style', 'font-size: 20px; color: red');
+            message = "Game over! You won :)";
+        } else if (computerScore>=5){
+            gameOver = true;
+            document.getElementById('results').setAttribute('style', 'font-size: 20px; color: red');
+            message = "Game over! You lost :(";
+        }
+    }
+    return(message);
+
 }
 
 const results = document.querySelector('#results');
